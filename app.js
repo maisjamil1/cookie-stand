ed43a130798702582720a27680e80e066efdeb54
maisjamil1 <maisjamil17118@gmail.com>
2020-02-23 22:47:27 +0200

    5

--- app.js
+++ app.js
@@ -13,25 +13,31 @@ var seattle = {
   maxCust: 65,
   avgCookie: 6.3,
   hours: ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"],
-  customers: [0],
-  cookieNumber: [0],
+  customers: [],
+  cookieAmounts: [],
+  totalCookie:0,
   randomCust$$: function () {
     for (var i = 0; i < this.hours.length; i++) {
       this.customers[i] = getRandomCust(this.minCust, this.maxCust);
-      // console.log(this.customers);
+      
 
     }
   },
   cookieCount: function () {
     for (var j = 0; j < this.hours.length; j++) {
-      this.cookieNumber[j] = this.customers[j] * this.avgCookie;
-      this.cookieNumber[j]=Math.floor(this.cookieNumber[j]);
-      // console.log(this.cookieNumber);
+      this.cookieAmounts[j] = this.customers[j] * this.avgCookie;
+      this.cookieAmounts[j]=Math.floor(this.cookieAmounts[j]);
+      
+    }
+  },
+  findCookieTotal:function(){
+    for(var c=0;c<this.cookieAmounts.length;c++){
+      this.totalCookie=this.totalCookie+this.cookieAmounts[c];
     }
   }
-
 
 }
 seattle.randomCust$$();
 seattle.cookieCount();
+seattle.findCookieTotal();
 console.log(seattle);
